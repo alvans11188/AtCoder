3a9fc25081e24b1a1c3ef8c6de22d78d400d4c31
Alvans <achicallag@unjbg.edu.pe>
2025-08-12 00:07:34 -0500

    agregando archivos

--- 3abc283b/abc283b.cpp
+++ 3abc283b/abc283b.cpp
@@ -3,6 +3,24 @@
 using namespace std;
 
 int main(){
-	
+	int n;cin>>n;
+
+	vector<int> arreglo;
+	for(int i=0;i<n;i++){
+		int dato;cin>>dato;
+		arreglo.push_back(dato);
+	}
+	int consultas;cin>>consultas;
+	while(consultas--){
+		int q;cin>>q;
+		int valor;cin>>valor;
+		if(q==2) cout<<arreglo[valor-1]<<endl;
+		else {
+			int data;cin>>data;
+			arreglo[valor-1]=data;
+		}
+			
+		
+	}
 	return 0;
 }
